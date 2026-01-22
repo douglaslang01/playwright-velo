fc87cf26b9f519bfb06ede40513b5534aa988293
douglaslang01 <douglas.lang@outlook.com>
2026-01-22 17:43:53 -0300

    Adicionando padrão AAA ao teste

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,18 +1,21 @@
 import { test, expect } from '@playwright/test';
 
+// AAA - Arrange, Act, Assert
+
 test('deve consultar um pedido aprovado', async ({ page }) => {
+    // Arrange
     await page.goto('http://localhost:5173/');
     // Checkpoint 1: Verificar se a página de consulta de pedidos está carregada
     await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
-
     await page.getByRole('link', { name: 'Consultar Pedido' }).click();
     // Checkpoint 2: Verificar se a página de consulta de pedidos está carregada
     await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
+    // Act
     await page.getByTestId('search-order-id').fill('VLO-85DC4D');
     await page.getByTestId('search-order-button').click();
 
-    // Checkpoint 3: Verificar se o pedido foi encontrado
+    // Assert
     await expect(page.getByTestId('order-result-id')).toContainText('VLO-85DC4D');
     await expect(page.getByTestId('order-result-status')).toBeVisible();
     await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
