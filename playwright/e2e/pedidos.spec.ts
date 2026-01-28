033598266aecd76d49bba94ae606b751e525a4cf
douglaslang01 <douglas.lang@outlook.com>
2026-01-28 17:11:02 -0300

    Isolando Dados de Teste

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -3,6 +3,9 @@ import { test, expect } from '@playwright/test';
 // AAA - Arrange, Act, Assert
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
+    // Test data
+    const order = 'VLO-85DC4D'
+
     // Arrange
     await page.goto('http://localhost:5173/');
     // Checkpoint 1: Verificar se a página de consulta de pedidos está carregada
@@ -12,18 +15,18 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
     await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
     // Act
-    await page.getByTestId('search-order-id').fill('VLO-85DC4D');
+    await page.getByTestId('search-order-id').fill(order);
     // await page.getByTestId('search-order-button').click();
     await page.getByRole('button', { name: 'Buscar Pedido' }).click();
 
     // Assert
-    // const orderCode = page.locator('//p[text()="Pedido"]/..//p[text()="VLO-85DC4D"]');
+    //const orderCode = page.locator(`//p[text()="Pedido"]/..//p[text()="${order}"]`);
     // await expect(orderCode).toBeVisible({ timeout: 10_000 });
 
     const containerPedido = page.getByRole('paragraph')
         .filter({ hasText: /^Pedido$/ })
         .locator('..'); // Sobe para o elemento pai (div que agrupa ambos)
-    await expect(containerPedido).toContainText('VLO-85DC4D', { timeout: 10_000 });
+    await expect(containerPedido).toContainText(order, { timeout: 10_000 });
 
     await expect(page.getByText('APROVADO')).toBeVisible();
 });
