04592d05eb147f5e0da6f2be74cd0b8af30491af
douglaslang01 <douglas.lang@outlook.com>
2026-09-23 11:42:24 -0300

    Implementado estágio de regressao dos testes regressivos em CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,8 +35,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'https://playwright-velo-djzr6m9jy-douglas-lang.vercel.app/',
-    //baseURL: 'http://localhost:5173',
+    //baseURL: 'https://playwright-velo-djzr6m9jy-douglas-lang.vercel.app',
+    baseURL: process.env.BASE_URL || 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -85,9 +85,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'yarn dev',
-  //   url: 'http://localhost:5173',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'yarn dev',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
+  },
 });
